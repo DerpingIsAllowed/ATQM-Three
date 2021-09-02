c0fc3fa838f6846e5e6e7db33cac9ddc1d382471
MDM-resourcepack <75644452+MDM-resourcepack@users.noreply.github.com>
2021-09-02 17:42:32 +0200

    map threejjs
    dada

---
+++ three.js/examples/jsm/renderers/webgl/nodes/WebGLNodes.js
@@ -0,0 +1,35 @@
+import { WebGLNodeBuilder } from './WebGLNodeBuilder.js';
+import NodeFrame from '../../nodes/core/NodeFrame.js';
+
+import { Material } from 'three';
+
+const builders = new WeakMap();
+export const nodeFrame = new NodeFrame();
+
+Material.prototype.onBuild = function ( parameters, renderer ) {
+
+	builders.set( this, new WebGLNodeBuilder( this, renderer, parameters ).build() );
+
+};
+
+Material.prototype.onUpdate = function ( renderer, scene, camera, geometry, object ) {
+
+	const nodeBuilder = builders.get( this );
+
+	if ( nodeBuilder !== undefined ) {
+
+		nodeFrame.update();
+
+		nodeFrame.material = this;
+		nodeFrame.camera = camera;
+		nodeFrame.object = object;
+
+		for ( const node of nodeBuilder.updateNodes ) {
+
+			nodeFrame.updateNode( node );
+
+		}
+
+	}
+
+};
