2820c254ccf9fdc0327a11aae4bc738ac3c78638
Steppie <holyblobvish@gmail.com>
2021-09-04 23:23:53 +0200

    Helpers gescheiden & CSS

--- index2.js
+++ index2.js
@@ -1,13 +1,14 @@
 import * as THREE from '../three.js/build/three.module.js';
-import { BoxHelper, CullFaceBack, PlaneHelper, QuadraticBezierCurve, WireframeGeometry } from './three.js/build/three.module.js';
+import { BoxHelper, CullFaceBack, PlaneHelper, QuadraticBezierCurve, TriangleFanDrawMode, WireframeGeometry } from './three.js/build/three.module.js';
 
 import {OrbitControls} from './three.js/examples/jsm/controls/OrbitControls.js';
+import { GUI } from './three.js/examples/jsm/libs/dat.gui.module.js';
 
 //initiate some variables
 let scene, camera, renderer, controls;
 
 //initiate custom variables
-let camerazoom, EnableHelpers, BackgroundColor, EnableClipping, clipPlanes;
+let camerazoom, EnableLightHelpers, EnableClippingHelpers, BackgroundColor, EnableClipping, clipPlanes, ClippingPlaneOfset;
 
 
 init();
@@ -22,10 +23,12 @@ function init() {
     */
 
     //boleans and stuff for in the UI and easy acces    
-    EnableHelpers = true;
+    EnableLightHelpers = false;
+    EnableClippingHelpers = false;
     camerazoom = 1.5;
     BackgroundColor = 0xffffff;
     EnableClipping = true;
+    ClippingPlaneOfset = 0;
 
     //define a scene
     scene = new THREE.Scene();
@@ -47,9 +50,9 @@ function init() {
         
         //add in the three clipping planes that make-up the box
         clipPlanes = [
-            new THREE.Plane( new THREE.Vector3( 1, 0, 0 ), 0 ),
-            new THREE.Plane( new THREE.Vector3( 0, -1, 0 ), 0 ),
-            new THREE.Plane( new THREE.Vector3( 0, 0, -1 ), 0 )
+            new THREE.Plane( new THREE.Vector3( -1, 0, 0 ), ClippingPlaneOfset ),
+            new THREE.Plane( new THREE.Vector3( 0, -1, 0 ), ClippingPlaneOfset ),
+            new THREE.Plane( new THREE.Vector3( 0, 0, -1 ), ClippingPlaneOfset )
         ];
         renderer.localClippingEnabled = true;
     }
@@ -103,7 +106,7 @@ function init() {
     controls.enablePan=false;
     //automagically rotate
     controls.autoRotate=true;
-    controls.autoRotateSpeed=1;
+    controls.autoRotateSpeed=0;
 
     //lighting
     // enable helpers at the top to see what you're doing!
@@ -132,7 +135,7 @@ function init() {
     scene.add(lightD);
     scene.add(lightD.target);
 
-    if (EnableHelpers == true)
+    if (EnableLightHelpers == true)
     {   //light visualizers
         const helper = new THREE.BoxHelper(lightD.target, 0xFF0000 );
         scene.add(helper);
@@ -148,13 +151,14 @@ function init() {
 
         const helperD2 = new THREE.DirectionalLightHelper( lightD2, 5, 0xFF0000 );
         scene.add(helperD2);
+    }
 
-
-        //clippingplane helpers
+    if (EnableClippingHelpers == true)
+    {       //clippingplane helpers
         const planehelpers= [
             new PlaneHelper(clipPlanes[0], 10 ,0xFF0000),
             new PlaneHelper(clipPlanes[1], 10 ,0xFF0000),
-            new PlaneHelper(clipPlanes[2], 10 ,0xFF0000)
+            new PlaneHelper(clipPlanes[2], 10 ,0xFF0000)      
         ];
         
         scene.add(planehelpers[0]);
